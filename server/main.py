284947097c849abc987e42c490109a1ba42b58f8
Ian McCann <mcian91@gmail.com>
2023-03-28 18:33:07 -0700

    first

--- server/main.py
+++ server/main.py
@@ -162,4 +162,4 @@ async def startup():
 
 
 def start():
-    uvicorn.run("server.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("server.main:app", host="0.0.0.0", port=8000, reload=True, workers=8)
